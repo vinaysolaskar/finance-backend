220e8a0e9ff31cb2c066805cd5cd0628b7c3241f
Vinay Solaskar <vinaysolaskar1@gmail.com>
2026-04-03 18:21:19 +0530

    feat: logging, global exception handling, validation, and middleware setup

---
+++ src/logger.module.ts
@@ -0,0 +1,44 @@
+import { Module } from '@nestjs/common';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
+
+@Module({
+    imports: [
+        WinstonModule.forRoot({
+            level: 'info',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.errors({ stack: true }),
+                winston.format.json(),
+            ),
+            transports: [
+                new winston.transports.Console({
+                    format: winston.format.combine(
+                        winston.format.colorize(),
+                        winston.format.printf(
+                            ({ timestamp, level, message, context, ...meta }) =>
+                                `${timestamp} [${level}] [${context || 'App'}] ${message} ${Object.keys(meta).length ? JSON.stringify(meta) : ''
+                                }`,
+                        ),
+                    ),
+                }),
+
+                new DailyRotateFile({
+                    filename: 'logs/app-%DATE%.log',
+                    datePattern: 'YYYY-MM-DD',
+                    maxFiles: '14d',
+                }),
+
+                new DailyRotateFile({
+                    filename: 'logs/error-%DATE%.log',
+                    level: 'error',
+                    datePattern: 'YYYY-MM-DD',
+                    maxFiles: '30d',
+                }),
+            ],
+        }),
+    ],
+    exports: [WinstonModule],
+})
+export class LoggerModule { }
