af331852ffd18445a3f3169b93f83b454e4b3481
Vinay Solaskar <vinaysolaskar1@gmail.com>
2026-04-05 20:30:25 +0530

    fix: update redis implementation in env

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -10,17 +10,7 @@ dotenv.config();
         {
             provide: 'REDIS_CLIENT',
             useFactory: () => {
-                const client = new Redis({
-                    host: process.env.REDIS_HOST,
-                    port: Number(process.env.REDIS_PORT),
-                    password: process.env.REDIS_PASSWORD,
-                    retryStrategy: (times) => {
-                        if (times > 2) return null;
-                        return 500;
-                    },
-
-                    reconnectOnError: () => false,
-                });
+                const client = new Redis(process.env.REDIS_URL!);
 
                 client.on('connect', () => console.log('Redis connected'));
                 client.on('error', (err) => console.error('Redis error', { error: err }));
